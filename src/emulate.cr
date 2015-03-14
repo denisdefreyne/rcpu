0c06304627b06fd72a20c7d9c778578df481d9cd
Denis Defreyne <denis.defreyne@stoneship.org>
2015-03-14 23:33:45 +0100

    Removed unused lib block

--- src/emulate.cr
+++ src/emulate.cr
@@ -1,11 +1,6 @@
 #!/usr/bin/env ruby
 # encoding: utf-8
 
-@[Link("SDL2_gfx")]
-lib LibSDL2_GFX
-  fun box_color = boxColor(dst : Void*, x1 : Int16, y1 : Int16, x2 : Int16, y2 : Int16, color : UInt32) : Int32
-end
-
 require "sdl2"
 
 class Registers
