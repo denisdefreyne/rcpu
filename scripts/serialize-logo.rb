5fd870f3d7f52737cc37b9b527f7b352e0f0d540
Denis Defreyne <denis@soundcloud.com>
2016-08-12 16:50:20 +0200

    Use codemotion logo

--- scripts/serialize-logo.rb
+++ scripts/serialize-logo.rb
@@ -1,4 +1,10 @@
 require 'chunky_png'
 
-image = ChunkyPNG::Image.from_file('/Users/ddfreyne/Desktop/soundcloud-logo-bw.png')
+if ARGV.size != 1
+  $stderr.puts "usage: #{$0} [filename]"
+  exit 1
+end
+filename = ARGV[0]
+
+image = ChunkyPNG::Image.from_file(filename)
 puts image.pixels.map { |pixel| pixel <= 255 ? 0 : 1 }.map { |i| ".byte 0x0#{i}" }.join("\n")
